118a1617514a07e6edc4030ef90e9424dd885807
JamisonHolt <JamisonHolt95@gmail.com>
2018-10-10 16:03:22 -0500

    Somewhat decent segmentation

--- Main.py
+++ Main.py
@@ -17,8 +17,9 @@ def sitk_show(img, title=None, margin=0.05, dpi=40):
 
     nda = SimpleITK.GetArrayFromImage(img)
     spacing = img.GetSpacing()
-    figsize = (1 + margin) * nda.shape[0] / dpi, (1 + margin) * nda.shape[1] / dpi
+    # figsize = (1 + margin) * nda.shape[0] / dpi, (1 + margin) * nda.shape[1] / dpi
     extent = (0, nda.shape[1] * spacing[1], nda.shape[0] * spacing[0], 0)
+    figsize = (5, 5)
     fig = plt.figure(figsize=figsize, dpi=dpi)
     ax = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
 
@@ -27,8 +28,6 @@ def sitk_show(img, title=None, margin=0.05, dpi=40):
 
     if title:
         plt.title(title)
-
-    plt.show()
 
 
 def main():
@@ -61,7 +60,7 @@ def main():
     print(imgOriginal.GetOrigin())
     print(type(imgOriginal) == SimpleITK.SimpleITK.Image)
 
-    imgOriginal = imgOriginal[:, :, idxSlice]
+    # imgOriginal = imgOriginal[:, :, idxSlice]
     imgSmooth = SimpleITK.CurvatureFlow(image1=imgOriginal,
                                         timeStep=0.125,
                                         numberOfIterations=10)
@@ -73,19 +72,25 @@ def main():
 
     # sitk_show(imgSmooth)
 
-    lstSeeds = [(30, 30)]
+    lstSeeds = [(28, 31, 37), (22, 12, 22), (50, 40, 22), (28, 31, 37)]
 
     imgWhiteMatter = SimpleITK.ConnectedThreshold(image1=imgSmooth,
                                                   seedList=lstSeeds,
-                                                  lower=350,
-                                                  upper=1000,
+                                                  lower=300,
+                                                  upper=459,
                                                   replaceValue=labelWhiteMatter)
 
     # Rescale 'imgSmooth' and cast it to an integer type to match that of 'imgWhiteMatter'
     imgSmoothInt = SimpleITK.Cast(SimpleITK.RescaleIntensity(imgSmooth), imgWhiteMatter.GetPixelID())
 
-    # Use 'LabelOverlay' to overlay 'imgSmooth' and 'imgWhiteMatter'
-    sitk_show(SimpleITK.LabelOverlay(imgSmoothInt, imgWhiteMatter))
+    for i in range(imgOriginal.GetDepth()):
+        imgWhiteMatterSingle = imgWhiteMatter[:, :, i]
+        imgSmoothIntSingle = imgSmoothInt[:, :, i]
+        # Use 'LabelOverlay' to overlay 'imgSmooth' and 'imgWhiteMatterSingle'
+        sitk_show(SimpleITK.LabelOverlay(imgSmoothIntSingle, imgWhiteMatterSingle))
+        # sitk_show(imgSmoothIntSingle)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
